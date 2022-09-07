949c822b347f5548bb9dd9ea46e13568d18112e1
Ben <byeungis@gmail.com>
2022-09-06 23:58:20 -0400

    Update the Product List Component

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_CATEGORIES } from "../../utils/queries";
+import {
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from "../../utils/actions";
+import { useStoreContext } from "../../utils/GlobalState";
 
-function CategoryMenu({ setCategory }) {
+function CategoryMenu() {
+  //useStoreContext() Hook to retrieve the current state form the global state object.  the dispatch() method to update state
+  const [state, dispatch] = useStoreContext();
+
+  //categories array out of our global state, simply destructe it out of state so we can use it to provie to our returning JSX
+  const { categories } = state;
+
+  //since we currently dont have data in state yet, we take the categoryData that returns from the useQuery() Hook and use the dispatch() method to set our cglobal state.  need to use useEffect() Hook
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
+
+  //useEffect() hook takes two arguments function to run given a certain condition and then the condition
+  useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories,
+      });
+    }
+  }, [categoryData, dispatch]);
+
+  const handleClick = (id) => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id,
+    });
+  };
 
   return (
     <div>
@@ -13,7 +43,7 @@ function CategoryMenu({ setCategory }) {
         <button
           key={item._id}
           onClick={() => {
-            setCategory(item._id);
+            handleClick(item._id);
           }}
         >
           {item.name}
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,29 +1,47 @@
-import React from "react";
+//displays the product's list
+
+import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 
 import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif";
 
-function ProductList({ currentCategory }) {
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+
+function ProductList() {
+  const [state, dispatch] = useStoreContext();
+
+  const { currentCategory } = state;
+
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  //useEffect() hook is used in order to wait for our useQuery() response to come in.  once the data object returned from useQuery() goes from undefined to having an actual value, we exectue our dispatch() function, instructing our reducer function that it's the UPDATE_PRODUCTS action and it should save the aray of product data to our global store.
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+    }
+  }, [data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
-      return products;
+      return state.products;
     }
 
-    return products.filter(
+    return state.products.filter(
       (product) => product.category._id === currentCategory
     );
   }
 
+  //updated products.length to state.products.length because we are now retriving products from the state object
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {products.length ? (
+      {state.products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
