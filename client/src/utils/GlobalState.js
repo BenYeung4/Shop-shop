402f79fa40765b663c1a1fc4c3c8b46f9801364a
Ben <byeungis@gmail.com>
2022-09-06 23:33:55 -0400

    context API added

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,6 +1,11 @@
+//React Context API, redux-like feature
+
+//createContext will be used to instantiate a new context object.  using it to create the container to hold our global state data and functionality so we can provid it throughout the app
+//useContext is another React Hook that will allow us to use th state created from the createCotnext function
 import React, { createContext, useContext } from "react";
 import { useProductReducer } from "./reducers";
 
+//every context object comes with two components, a provider and consumer.  provider is a special type of react componet that we wrap our application in so it can make the state data thats pased inot it as a proper avialable to all other compnents.  Consumer is our means of grabbing and using thedata that th provider holds for us
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
